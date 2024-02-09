86b2e03d9137eb350716dea30fa709ce23d9c230
jungeun5-choi <jungeun5.choi@gmail.com>
2024-02-09 20:49:27 +0900

    init: essential files

---
+++ src/main/java/com/team5/collection/CollectionApplication.java
@@ -0,0 +1,11 @@
+package com.team5.collection;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CollectionApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(CollectionApplication.class, args);
+    }
+}
